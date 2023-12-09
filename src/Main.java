a37a6ce0abbe2074c08152040b44d002aa555901
user <ilya.bubnoff@yandex.ru>
2023-12-09 20:48:13 +0300

    solving 8 problems

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+var dog = 8.0;
+var cat = 3.6;
+var paper = 763789;
+System.out.println(dog);
+System.out.println(cat);
+System.out.println(paper);
+
+var dog2 = dog+4;
+var cat2 = cat+4;
+var paper2 = paper+4;
+System.out.println(dog2);
+System.out.println(cat2);
+System.out.println(paper2);
+var dog3 = dog-3.5;
+var cat3 = cat-1.6;
+var paper3 = paper-7639;
+System.out.println(dog3);
+System.out.println(cat3);
+System.out.println(paper3);
+var friend = 19;
+        System.out.println(friend);
+ var friend2 = friend+2;
+        System.out.println(friend2);
+  var friend3 = friend2/7;
+        System.out.println(friend3);
+   var frog = 3.5;
+        System.out.println(frog);
+    var frog2 = frog*10;
+        System.out.println(frog2);
+var frog3 = frog2/3.5;
+        System.out.println(frog3);
+var frog4 = frog3+4;
+        System.out.println(frog4);
+var boxerisMass1 = 78.2;
+var boxerisMass2 = 82.7;
+var totalWeight = boxerisMass1+boxerisMass2;
+        System.out.println(totalWeight);
+var massDifference = boxerisMass2-boxerisMass1;
+        System.out.println(massDifference);
+var massDifference2 = boxerisMass2%boxerisMass1;
+        System.out.println(massDifference2);
+var sumofHors = 640;
+var openingHours = 8;
+var amountofWorkers = sumofHors/openingHours;
+        System.out.println("Всего работников в компании " + amountofWorkers + " человек");
+var amountofWorkers2 = amountofWorkers+94;
+var sumofHors2 = amountofWorkers2*openingHours;
+        System.out.println("Если в компании работает " + amountofWorkers2 + " человек, то всего " + sumofHors2 + " часов работы может быть поделено между сотрудниками");
+
+
     }
 }
